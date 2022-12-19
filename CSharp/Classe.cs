77f566893eb8389ff3d9b4487d50bab3360998da
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2022-12-18 21:03:02 -0300

    CSharp Intro

---
+++ CSharp/Classe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp;
+class Pessoa
+{
+    public string Nome { get; set; }
+    public string Estado { get; set; }
+    public int Idade { get; set; }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Primeiro;
+using CSharp;
+using Interface;
+using Enum;
+
+namespace CSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pessoa = (Pessoas)0;
+
+        Pessoas pessoa2 = Pessoas.Joao;
+
+        Pessoas pessoa3 = (Pessoas)4;
+
+        Pessoa person = new Pessoa();
+
+        person.Nome = "Welinton";
+        person.Idade = 18;
+        person.Estado = "SP";
+
+        var person2 = new Pessoa();
+
+        person2.Nome = "Giovanna";
+        person2.Idade = 18;
+        person2.Estado = "SP";
+
+        Animal animal = new Animal();
+
+        animal.Nome = "Pipoca";
+        animal.NomeDono = "Welinton";
+        animal.Especie = "Cachorro"; 
+
+        var classe = new Classe();
+        var classe2 = new Segundo.Classe();
+
+        Console.WriteLine("Hello World!");
+
+        Console.WriteLine(pessoa3);
+    }
+}
